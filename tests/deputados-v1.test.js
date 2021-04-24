0ff2fed0fd8196ee3b3782c5e80a7aabb839143b
Andre Monello <atmonello@outlook.com>
2021-04-24 17:10:27 -0300

    test(deputados): finish tests file covering other listing methods

--- tests/deputados-v1.test.js
+++ tests/deputados-v1.test.js
@@ -20,4 +20,109 @@ describe('/camara/v1/deputados', () => {
       ]),
     });
   });
+
+  test('Requisitando deputados de um estado válido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/uf/SP`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.data).toMatchObject({
+      status: 'ok',
+      deputados: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          siglaPartido: expect.any(String),
+          siglaUf: expect.any(String),
+          idLegislatura: expect.any(Number),
+          urlFoto: expect.any(String),
+          email: expect.any(String),
+        }),
+      ]),
+    });
+  });
+
+  test('Requisitando deputados de um estado inválido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/uf/123`;
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+      expect(response.status).toBe(404);
+      expect(response.data).toMatchObject({
+        name: 'NotFoundError',
+        message: 'UF não encontrado.',
+        type: 'not_found',
+      });
+    }
+  });
+
+  test('Requisitando deputados de um partido válido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/partido/PP`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.data).toMatchObject({
+      status: 'ok',
+      deputados: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          siglaPartido: expect.any(String),
+          siglaUf: expect.any(String),
+          idLegislatura: expect.any(Number),
+          urlFoto: expect.any(String),
+          email: expect.any(String),
+        }),
+      ]),
+    });
+  });
+
+  test('Requisitando deputados de um partido inválido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/partido/123`;
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+      expect(response.status).toBe(404);
+      expect(response.data).toMatchObject({
+        name: 'NotFoundError',
+        message: 'Partido não encontrado.',
+        type: 'not_found',
+      });
+    }
+  });
+
+  test('Requisitando deputados com um ID válido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/160508`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.data).toMatchObject({
+      status: 'ok',
+      deputados: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          siglaPartido: expect.any(String),
+          siglaUf: expect.any(String),
+          idLegislatura: expect.any(Number),
+          urlFoto: expect.any(String),
+          email: expect.any(String),
+        }),
+      ]),
+    });
+  });
+
+  test('Requisitando deputados com um ID inválido', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/camara/v1/deputados/111111111`;
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+      expect(response.status).toBe(404);
+      expect(response.data).toMatchObject({
+        name: 'NotFoundError',
+        message: 'Deputado não encontrado.',
+        type: 'not_found',
+      });
+    }
+  });
 });
